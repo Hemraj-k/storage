741eb02e6ea47abf8db8b910df6119d1a9c3000c
Hemraj-k <83349808+Hemraj-k@users.noreply.github.com>
2022-10-16 17:31:50 +0530

    w

--- cookies/src/Views/login/login.tsx
+++ cookies/src/Views/login/login.tsx
@@ -3,8 +3,26 @@ import Cookies from 'universal-cookie';
 import { useEffect, useState } from 'react';
 const Login = () => {
   const cookies = new Cookies();
-  const LoginHandler = () => {
+  const LoginHandler = (e: any) => {
+    e.preventDefault();
     cookies.set('auth', 'true', { path: '/' });
+    // window.location.reload();
+
+    fetch('http://localhost:3004/users')
+      .then((response) => response.json())
+      .then((data) => {
+        for (let i = 0; i < data.length; i++) {
+          if (data[i].name === e.target.name.value) {
+            if (data[i].password === e.target.password.value) {
+              cookies.set('auth', 'true', { path: '/' });
+              window.location.reload();
+            }
+          }
+        }
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
   };
 
   const submitHandler = (e: any) => {
@@ -39,10 +57,10 @@ const Login = () => {
         <button>Register</button>
       </form>
       Login
-      <form method="post">
-        {/* <input type="text" className="input" name="email" />
-        <input type="password" className="input" name="password" /> */}
-        <button onClick={LoginHandler}>Login</button>
+      <form onSubmit={LoginHandler}>
+        <input type="text" className="input" name="name" />
+        <input type="password" className="input" name="password" />
+        <button>Login</button>
       </form>
     </div>
   );
